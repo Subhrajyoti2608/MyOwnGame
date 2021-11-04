4abfaceb4772e028c8b8040b9fabb8e5486e9b0f
Subhrajyoti2608 <86191223+Subhrajyoti2608@users.noreply.github.com>
2021-11-04 09:04:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,10 +2,10 @@ var boy;
 var lion;
 var jungle, stone, wood;
 
-var PLAY=1;
-var END=0;
-var gameState=1;
-
+//var PLAY=1;
+//var END=0;
+//var gameState=PLAY;
+var edges
 function preload(){
   boyImg = loadImage("assets/boy.png")
   jungleImg = loadImage("assets/jungle.png")
@@ -19,18 +19,23 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(1200,600);
+  createCanvas(900,600);
+  jungle=createSprite(300,300,1200,600)
+  jungle.addImage("abc",jungleImg)
+  jungle.scale = 3.3
+  jungle.velocityX=-3
   boy = createSprite(700, 420, 10, 10);
   boy.addImage("abc",boyImg)
   boy.Scale = 0.01
+
+  edges = createEdgeSprites()
   
   lion = createSprite(200, 500, 50, 50);
   lion.addImage("abc", lionImg)
   lion.scale=0.8
 
-  //jungle=createSprite(1200,600)
-  //jungle.addImage("abc",jungleImg)
-  //score = 0;
+  
+  score = 0;
 
   //boy.setCollider()
 
@@ -40,16 +45,18 @@ function setup() {
 }
 
 function draw() {
-  background(jungleImg); 
+  background(0); 
   
-  if(gameState === PLAY){
+  //if(gameState === PLAY){
 
-    Stone;
-    Wood;
+    Stone()
+    Wood()
     
-    jungle.velocityX = 10
-    lion.velocityX = 6
-    
+    //jungle.velocityX = 10
+    lion.velocityX = 3
+    //if(edges.isTouching(boy)){
+      boy.bounceOff(edges)
+    //}
 
    if(keyIsDown(UP_ARROW)){
       boy.velocityY = -16
@@ -59,43 +66,60 @@ function draw() {
      woodGroup.destroyEach()
       score = score+1
     
-}
+//}
 
     if(stoneGroup.isTouching(boy)){
-     gameState = END
-
+     //gameState = END
+     lion.velocityX = 0
+     boy.velocityX = 0
+     jungle.velocityX = 0
+     woodGroup.setVelocityXEach(0)
+     stoneGroup.setVelocityXEach(0)
     }
   
-  if(keyIsDown(RIGHT_ARROW)){
-    boy.velocityX = 8
+  //if(keyIsDown(RIGHT_ARROW)){
+    boy.velocityX = 10
     breathingSound.play()
-  }
+ // }
   if(lion.isTouching(boy)){
     lionGrowl.play()
-    gameState = END
-  }
-}
-else if(gameState === END){
-  lion.velocityX = 0
+    //gameState = END
+    lion.velocityX = 0
   boy.velocityX = 0
   jungle.velocityX = 0
   woodGroup.setVelocityXEach(0)
   stoneGroup.setVelocityXEach(0)
-
   fill (25)
   stroke("red")
   textSize(25)
   text("You lost",600,400)
+  }
+}
+//else if(gameState === END){
+  //lion.velocityX = 0
+  //boy.velocityX = 0
+  //jungle.velocityX = 0
+  //woodGroup.setVelocityXEach(0)
+  //stoneGroup.setVelocityXEach(0)
+
+  //fill (25)
+  //stroke("red")
+ // textSize(25)
+  //text("You lost",600,400)
+//}
+if(jungle.x<0){
+  jungle.x=jungle.width/8
 }
 
   drawSprites();
 }
 
 function Stone(){
-  if(World.frameCount%200===0){
-    stone = createSprite(600, 400, 10, 10)
+  if(World.frameCount%100===0){
+    stone = createSprite(50, 800, 5, 5)
     stone.addImage("abc",stoneImg)
-    stone.x = Math.round(random(100,400))
+    stone.scale = 0.1
+    stone.x = Math.round(random(50,50))
     stone.velocityX = -(8+(score/10))
     stone.setLifetime=50;
 
